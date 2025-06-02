16cbf8f189b274e5754cd0f93719d89ee52d9d11
deJonY <116846445+deJonY@users.noreply.github.com>
2025-06-02 19:09:46 +0500

    update

--- mini-twitter-frontend/src/App.jsx
+++ mini-twitter-frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./pages/Home";
 import MyPosts from "./pages/MyPosts";
 import MyLikes from "./pages/MyLikes";
 import Profile from "./pages/Profile";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
